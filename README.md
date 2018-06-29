c947ec6f63d38002490cbc40cbf42e43841fb091
gruposeven <marcospaulo.oliveira@gruposeven.org>
2018-06-29 16:22:37 -0300

    atualização da pagina principal

---
+++ README.md
@@ -0,0 +1,3 @@
+Criação do site e sistema Condomínio Total Ville 101 
+
+Criação da base inicial com a informação "sistema em implantação"
---
+++ index.php
@@ -0,0 +1,248 @@
+<html>
+	<head>
+		<title>Total Ville 101</title>
+		<meta charset="UTF-8">
+		<meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="biblioteca/normalize.css">
+		<link rel="shortcut icon" type="text/css" href="imagens/sistema.ico">
+		
+		<script type="text/javascript" src="script/script1.js"></script>
+		<script type="text/javascript" src="biblioteca/jquery-3.2.1.min.js"></script>
+
+		<body>
+
+			<!-- CABEÇALHO-->
+			<header>
+				<div class="container">
+					<div class="logo">
+					<img src="imagens/totalvillelogo.jpg">
+					</div>
+					<div class="menu">
+						<nav>
+							<div class="menu_mobile">
+								<div class="menu_mobile_linha">
+								</div>
+								<div class="menu_mobile_linha">
+								</div>
+								<div class="menu_mobile_linha">
+								</div>
+							</div>
+							<ul>
+								<li class="active"><a href="./"> Home</a></li>
+								<li><a href="./"> Reservas</a></li>
+								<li><a href="./"> Prestação de Contas</a></li>
+								<li><a href="./"> Assembléias</a></li>
+								<li><a href="./"> Taxa Condomínial</a></li>
+								<li><a href="./"> Negociações</a></li>
+								<li><a href="" target="_blanck"> Login </a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+		<!-- BANNER-->
+			<section id="banner">
+				<div class="container column">
+					<div class="anuncio_titulo">
+						<i><h1> Sistema em Implantação</h1></i>
+						<h2> Condômino, Atualize seu cadastro online para acessar o sistema</h2>
+					<a href="./"><div class="botao"><b>Cadastre-se...</b>
+					</div></a>
+					</div>
+
+				</div>
+			</section>
+					<!-- CORPO-->
+				<section id="corpo">
+					<div class="container">
+						<!-- Artigos-->
+						<article>
+							<div class="projeto">
+								<div class="projeto_titulo">Reserva de Churrasqueira 
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_imagem">
+									<img src="imagens/churrasqueira.jpg">
+								</div>
+								<div class="projeto_texto">
+									Realize a reserva de churrasqueira sem burocracia e com pagamento facilitado
+									por cartão de crédito ou com pagamento na próxima taxa condominial.
+								</div>	
+							</div>	
+							<div class="projeto">
+								<div class="projeto_titulo">Assembleia Mista Online 
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_imagem">
+									<img src="imagens/assembleia.png">
+								</div>
+								<div class="projeto_texto">
+									Participar e votar em Assembléia de Condomínio nunca ficou tão fácil. 
+									Com seu cadastro devidamente atualizado, você poderá participar de assembleias sem sair de casa.
+								</div>	
+							</div>	
+							<div class="projeto">
+								<div class="projeto_titulo">Reserva do Salão de Festa
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_imagem">
+									<img src="imagens/salaodefesta.jpg">
+								</div>
+								<div class="projeto_texto">
+								Realize a reserva do Salão de Festa sem burocracia e com pagamento facilitado
+								por cartão de crédito ou com pagamento na próxima taxa condominial.
+								</div>	
+							</div>	
+							<div class="projeto ">
+								<div class="projeto_titulo">Negociações
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_imagem">
+									<img src="imagens/negociacao.jpg">
+								</div>
+								<div class="projeto_texto">
+								Negocie suas taxas condominiais em atraso sem burocracia e com parcelamentos em até 12x. 
+								Pagamentos por cartão de crédito ou diretamente em boletos.	
+								</div>	
+							</div>	
+							<div class="projeto ">
+								<div class="projeto_titulo">Educação Esporte e Lazer
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_imagem">
+									<img src="imagens/eel.jpg">
+								</div>
+								<div class="projeto_texto">
+									Projeto exclusivo para Quadra 101 com atividades de esporte, cursos e eventos.
+									<br> Levando mais saúde e Qualidade de Vida para dentro do nosso condomínio
+								</div>	
+							</div>	
+							<div class="projeto ">
+								<div class="projeto_titulo">Classificados
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_imagem">
+									<img src="imagens/classificados.jpg">
+								</div>
+								<div class="projeto_texto">
+									Ofereça gratuitamente para vizinhança seus serviços e produtos e aumente sua renda.
+									Condôminos, o melhor produtos pode estar ao lado de sua casa. Conheça mais...
+								</div>	
+							</div>
+						
+						</article>
+
+						<!-- Coluna-->
+						<aside>
+							<div class="projeto coluna">
+								<div class="projeto_titulo">Documentos
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_filiadas">
+									
+									<div class="filiadas">
+										<b><center>Convenção Coletiva</center></b>
+										<img src="imagens/convencao.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Regimento Interno</center></b>
+										<img src="imagens/pasta3.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Planej. Orçamentário</center></b>
+										<img src="imagens/planejamento.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Atas de Assembléia</center></b>
+										<img src="imagens/assembleia2.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Responsável Legal</center></b>
+										<img src="imagens/responsavel.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Contratos Vigentes</center></b>
+										<img src="imagens/contratos.png">
+									</div>
+								</div>
+							</div>
+							<div class="projeto coluna">
+								<div class="projeto_titulo">Prestação de Contas
+								</div>
+								<div class="projeto_linha">
+								</div>
+								<div class="projeto_filiadas">
+									
+									<div class="filiadas">
+										<b><center>Receitas</center></b>
+										<img src="imagens/grafica.jpg">
+									</div>
+									<div class="filiadas">
+										<b><center>Despesas</center></b>
+										<img src="imagens/despesas.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Taxa Extra</center></b>
+										<img src="imagens/grafica2.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Folha de Pagamento</center></b>
+										<img src="imagens/folhapg2.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Inadimplência</center></b>
+										<img src="imagens/inadimplencia.png">
+									</div>
+									<div class="filiadas">
+										<b><center>Fundo de Reserva</center></b>
+										<img src="imagens/fundoreserva.jpg">
+									</div>
+								</div>
+							</div>
+
+						</aside>
+					</div>
+					
+				</section>
+			<!--Rodapé-->
+			<footer>
+				<div class="container">
+					<div class="contatos">
+							<div class="projeto_titulo">Endereço
+							</div>
+							<div class="projeto_linha">
+							</div>
+							<div class="enderecos">
+							Rua 600 Lote 601 Quadra 101 Setor Meirelles Santa Maria, Distrito Federal <br>
+							Telefone: <a href="tel:61-3395-6155">(61) 3395-6155</a>
+								</div>
+						
+					</div>
+					<div class="lista_servicos">
+							<div class="projeto_titulo">Departamentos
+							</div>
+							<div class="projeto_linha">
+							</div>
+								<a href="mailto:kelita.farias@totalville101.com.br">Síndico</a>
+								<a href="mailto:gabriel.fernandes@totalville101.com.br">Gerência Administrativa</a>
+								<a href="mailto:ariosvaldo.junior@totalville101.com.br">Administração</a>
+								<a href="mailto:conselho.fiscal@totalville101.com.br">Conselho Fiscal</a>
+					</div>
+				</div>
+			</footer>
+			<div class="copyright">
+				© Copyright 2017-2018 totalville101.com.br - All Rights Reserved
+			</div>	
+		</body>
+	</head>
+</html>
+
