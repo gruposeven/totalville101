0f61c31bfcad77e662f2c2c02d70e2f71e45febb
gruposeven <marcospaulo.oliveira@gruposeven.org>
2018-08-21 10:59:40 -0300

    Inicio da criação sistema/cadastro_pesso_fisica

---
+++ sistema/cadastro_pessoa_fisica.php
@@ -0,0 +1,369 @@
+<html>
+	<head>
+		<title>Total Ville 101</title>
+		<meta charset="UTF-8">
+		<meta id="viewport" name="viewport" content="width=device-width, user-scalable=no, shrink-to-fit=no">
+
+		<?php
+		require "../configuracoes.php";
+		?>
+
+
+		<link rel="stylesheet" type="text/css" href="../biblioteca/normalize.css">
+		<link rel="stylesheet" type="text/css" href="../biblioteca/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/styleStrap.css">
+
+
+		<link rel="shortcut icon" type="text/css" href="../imagens/sistema.ico">
+
+		<script type="text/javascript" src="../biblioteca/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="../biblioteca/bootstrap.bundle.min.js"></script>
+		<script type="text/javascript" src="script/script.js"></script>
+
+<?php
+				session_start();
+?>
+	</head>
+<!-- Modal Login Inicio-->
+<div class="modal fade" id="login">
+	<div class="modal-dialog modal-dialog-centered modal-sm"> 
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-titulo">Acesso ao Sistema</h5>
+				<button class="close" data-dismiss="modal">
+					<span>&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form method="POST" class="justify-content-around">
+					<div class="form-group">
+						<label for="login_usuario">Usuário:</label>
+						<input id="login_usuario" type="text" name="login_usuario" class="form-control 
+						form-control-sm" placeholder="Informe o CPF (somente números)"> 	
+					</div>
+					<div class="form-group">
+						<label for="login_senha">Senha:</label>
+						<input id="login_senha" type="password" name="login_senha" class="form-control 
+						form-control-sm" placeholder="Informe sua senha">
+					</div>
+					<div class="form-group"><center>
+						<input type="submit" value="Entrar" class="btn btn-primary"></center>
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer justify-content-between">
+				<a href="01cadastro.php"class="btn btn-success"target="_blank">Cadastra-se</a>
+				<a href="esqueci.php"class="btn btn-danger" target="_blank">Esqueci Senha</a>
+			</div>
+		</div>	
+	</div>
+</div>
+		<body>
+<!-- Navegação-->
+			<div class="container-fluid">
+<?php
+require "menu_nav_sistema.php"
+?>
+			</div>
+<!-- Término Navegação -->
+		<hr>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-4" id="corpo1">
+<?php
+require "menu_sistema.php";
+?>
+					</div>
+				<div class="col-8" id="corpo2">
+					<div class="row">
+						<div class="col-sm-9" id="corpo2A">
+							<div id="accordion">
+<?php
+$cpf="Informe o CPF";
+?>
+
+<?php
+require "validador_cadastro_pf.php";
+?>
+								<div class="card">
+									<div class="card-header justify-content-between">
+										<form id="form_validar_cpf" method="POST" 
+										enctype="multipart/form-data" 
+										class="form-inline">
+										<input id="validar_cpf" type="number" name="validar_cpf" class="form-control form-control-sm" 
+										placeholder="<?php echo $cpf;?>" disabled>								
+										<input id="botao_verificar" type="submit" class="btn
+										btn-sm" value="Verificar"disabled>
+										</form>
+										<button id="botao_cadastrar"value="Cadastrar" class="btn
+										btn-sm" data-toggle="collapse" aria-controls="card_cadastrar" 
+										data-target="#card_cadastrar" disabled>Cadastrar</button>
+										<button id="botao_editar" value="Editar" class="btn
+										btn-sm" data-toggle="collapse" aria-controls="card_editar" 
+										data-target="#card_editar" disabled>Editar</button>
+										<button id="botao_imprimir" value="Imprimir" class="btn
+										btn-sm" data-toggle="collapse" aria-controls="card_imprimir" 
+										data-target="#card_imprimir" disabled>Imprimir</button>
+										<button id="botao_deletar" value="Deletar" class="btn
+										btn-sm" data-toggle="collapse" aria-controls="card_editar"
+										data-target="#card_deletar" disabled>Deletar</button>
+
+									</form>
+									</div>
+								</div>
+<!-- Cadastrar-->
+								<div id="card_cadastrar" class="collapse" data-parent="#accordion">
+									<div class="card-body">
+										<form method="POST" enctype="multipart/form-data" class="form justify-content-between">
+											<div class="row">
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label for="dpnome_pf">Nome Completo:</label>
+														<input id="dpnome_pf" type="text" name="dpnome_pf" class="form-control 
+														form-control-sm"> 
+													</div>	
+													<div class="form-group">
+														<label for="dpcpf">CPF:</label>
+														<input id="dpcpf" type="number" name="dpcpf" class="form-control 
+														form-control-sm" placeholder="<?php echo $cpf;?>" readonly>
+													</div>
+													<div class="form-group">
+														<label for="dprg_PF">RG:</label>
+														<input id="dprg_pf" type="text" name="dprg_pf" class="form-control 
+														form-control-sm">
+													</div>
+													<div class="form-group">
+														<label for="dpnasc_PF">Data de Nascimento:</label>
+														<input id="dpnasc_pf" type="date" name="dpnasc_pf" class="form-control 
+														form-control-sm">
+													</div>
+													<div class="form-group">
+														<label for="dpemail_pf">E-mail:</label>
+														<input id="dpemail_pf" type="email" name="dpemail_pf" class="form-control 
+														form-control-sm">
+													</div>
+													<div class="form-group">
+														<label for="dpetelefone_pf">Telefone:</label>
+														<input id="dptelefone_pf" type="tel" name="dptelefone_pf" 
+														class="form-control	form-control-sm" placeholder="Informe 99999-9999">
+													</div>
+													<div class="form-group">
+														<label for="dpimagem_pf">Inserir Foto:</label><br>
+															<label class="btn btn-sm btn-warning">
+															Arquivo JPG/PNG 
+															<input class="btn btn-sm btn-warning form-control form-control-sm" type="file" 
+															id="dpimagem_pf" name="dpimagem_pf" hidden>
+															</label>	
+													</div>	
+												</div>
+											<div class="col-sm-6">
+												<div class="form-group">
+													<label for="dpendereco_rua_pf">Rua/Quadra/Setor:</label>
+													<input id="dpendereco_rua_pf" type="text" name="dpendereco_rua_pf" 
+													class="form-control form-control-sm">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_numero_pf">Número:</label>
+													<input id="dpendereco_numeropf" type="text" name="dpendereco_numero_pf" 
+													class="form-control form-control-sm">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_complemento_pf">Complemento:</label>
+													<input id="dpendereco_complemento_pf" type="text" name="dpendereco_complemento_pf"
+													class="form-control form-control-sm">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_bairro_pf">Bairro/Satélite:</label>
+													<input id="dpendereco_bairro_pf" type="text" name="dpendereco_bairro_pf" 
+													class="form-control form-control-sm">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_cidade_pf">Cidade:</label>
+													<input id="dpendereco_cidade_pf" type="text" name="dpendereco_cidade_pf" 
+													class="form-control form-control-sm">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_estado_pf">Estado:</label>
+													<input id="dpendereco_estado_pf" type="text" name="dpendereco_estado_pf" class="form-control 
+													form-control-sm">
+												</div>
+												<div class="form-group">
+													<label for="dpcep_pf">CEP:</label>
+													<input id="dpcep_pf" type="text" name="dpcep_pf" class="form-control form-control-sm">
+												</div>
+
+											</div>
+										</div>
+											<div class="form-group">
+												<input type="submit" value="Cadastrar" 
+												class="btn btn-success">
+											</div>
+										</form>	
+									</div>
+								</div>
+<!--Término Cadastrar-->								
+<!-- Editar-->
+								<div id="card_editar" class="collapse" data-parent="#accordion">
+									<div class="card-body">
+										<form method="POST" enctype="multipart/form-data" class="form justify-content-between">
+											<div class="row">
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label for="dpnome_pf">Nome Completo:</label>
+														<input id="dpnome_pf" type="text" name="dpnome_pf" class="form-control form-control-sm" placeholder="<?php echo $nome_pf;?>"> 
+													</div>	
+													<div class="form-group">
+														<label for="dpcpf">CPF:</label>
+														<input id="dpcpf" type="number" name="dpcpf" class="form-control 
+														form-control-sm" placeholder="<?php echo $cpf;?>" readonly>
+													</div>
+													<div class="form-group">
+														<label for="dprg_PF">RG:</label>
+														<input id="dprg_pf" type="text" name="dprg_pf" class="form-control form-control-sm" placeholder="<?php echo $rg_pf;?>">
+													</div>
+													<div class="form-group">
+														<label for="dpnasc_PF">Data de Nascimento:</label>
+														<input id="dpnasc_pf" type="text" name="dpnasc_pf" class="form-control form-control-sm" 
+														placeholder="<?php echo $nasc_pf3;?>" readonly>
+													</div>
+													<div class="form-group">
+														<label for="dpemail_pf">E-mail:</label>
+														<input id="dpemail_pf" type="email" name="dpemail_pf" class="form-control form-control-sm" 
+														placeholder="<?php echo $email_pf;?>">
+													</div>
+													<div class="form-group">
+														<label for="dpetelefone_pf">Telefone:</label>
+														<input id="dptelefone_pf" type="tel" name="dptelefone_pf" 
+														class="form-control	form-control-sm" 
+														placeholder="<?php echo $telefone_pf;?>">
+													</div>
+													<div class="form-group">
+														<label for="dpimagem_pf">Inserir Foto:</label><br>
+															<label class="btn btn-sm btn-primary">
+															Arquivo JPG/PNG 
+															<input class="btn btn-sm btn-primary form-control form-control-sm" type="file" 
+															id="dpimagem_pf" name="dpimagem_pf" hidden>
+															</label>	
+													</div>	
+												</div>
+											<div class="col-sm-6">
+												<div class="form-group">
+													<label for="dpendereco_rua_pf">Rua/Quadra/Setor:</label>
+													<input id="dpendereco_rua_pf" type="text" name="dpendereco_rua_pf"class="form-control form-control-sm" 
+													placeholder="<?php echo $endereco_rua_pf;?>">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_numero_pf">Número:</label>
+													<input id="dpendereco_numeropf" type="text" name="dpendereco_numero_pf" class="form-control form-control-sm"
+													placeholder="<?php echo $endereco_numero_pf;?>">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_complemento_pf">Complemento:</label>
+													<input id="dpendereco_complemento_pf" type="text" name="dpendereco_complemento_pf" class="form-control form-control-sm"
+													placeholder="<?php echo $endereco_complemento_pf;?>">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_bairro_pf">Bairro/Satélite:</label>
+													<input id="dpendereco_bairro_pf" type="text" name="dpendereco_bairro_pf"class="form-control form-control-sm"
+													placeholder="<?php echo $endereco_bairro_pf;?>">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_cidade_pf">Cidade:</label>
+													<input id="dpendereco_cidade_pf" type="text" name="dpendereco_cidade_pf" class="form-control form-control-sm"
+													placeholder="<?php echo $endereco_cidade_pf;?>">
+												</div>
+												<div class="form-group">
+													<label for="dpendereco_estado_pf">Estado:</label>
+													<input id="dpendereco_estado_pf" type="text" name="dpendereco_estado_pf" class="form-control form-control-sm"
+													placeholder="<?php echo $endereco_estado_pf;?>">
+												</div>
+												<div class="form-group">
+													<label for="dpcep_pf">CEP:</label>
+													<input id="dpcep_pf" type="text" name="dpcep_pf" class="form-control form-control-sm" 
+													placeholder="<?php echo $endereco_cep_pf;?>">
+												</div>
+
+											</div>
+										</div>
+											<div class="form-group">
+												<input type="submit" value="Editar" 
+												class="btn btn-warning">
+											</div>
+										</form>	
+									</div>
+								</div>
+<!--Término Editar-->
+<!--Deletar-->
+								<div id="card_deletar" class="collapse" data-parent="#accordion">
+									<div class="card-body">
+										<form method="POST" enctype="multipart/form-data" class="form justify-content-between">
+											<div class="row">
+												<div class="col-sm-6">
+													<div class="form-group">
+														<input id="dpnome_pf" type="text" name="confimacao_deletar" class="form-control form-control-sm" placeholder="CPF: <?php echo $cpf;?>"> 
+													</div>
+													<div class="form-group">
+														<input id="dpnome_pf" type="text" name="confimacao_deletar" class="form-control form-control-sm" placeholder="Nome: <?php echo $nome_pf;?>"> 
+													</div>
+												</div>
+												<div class="col-sm-6">
+													<div class="form-group">
+														<input id="dpnome_pf" type="text" name="confimacao_deletar" class="form-control form-control-sm" placeholder="RG: <?php echo $rg_pf;?>"> 
+													</div>
+													<div class="form-group">
+														<input id="dpnome_pf" type="text" name="confimacao_deletar" class="form-control form-control-sm" placeholder="Data de Nascimento: <?php echo $nasc_pf3;?>"> 
+													</div>
+												</div>
+											</div>
+													Esse Procedimento é irreversível - Todo o conteúdo será excluído
+													<hr>
+													<div class="form-group">
+														<label for="dpnome_pf">Informe sua senha:</label>
+														<input id="dpnome_pf" type="password" name="senha" class="form-control form-control-sm"> 
+													</div>	
+											<div class="form-group">
+												<input type="submit" value="Excluir" 
+												class="btn btn-danger">
+											</div>
+										
+										</form>
+									</div>
+								</div>
+
+
+
+							</div>
+						</div>
+						<div class="col-sm-3" id="corpo2B">
+							<div id ="alert_validacao"class="alert alert-sm alert-primary fade" 
+							role="alert">
+								<h6 class="alert-heading">Validação do CPF</h6>
+								<div class="alert alert-body"><p>Informe o número do CPF e clique em verificar para validação no Banco de Dados.</p></div>
+							</div> 
+							<div id ="alert_cpf_cadastrado"class="alert alert-sm alert-warning fade" 
+							role="alert">
+								<h6 class="alert-heading">CPF já cadastrado</h6>
+								<small>(<?php echo $cpf;?>)</small>								
+								<div class="alert alert-body"><p>O CPF informado já se encontra cadastrado em nosso sistema. Você poderá Editar os dados pessoais ou Deletar o cadastro</p></div>
+							</div> 
+							<div id ="alert_cpf_incorreto"class="alert alert-sm alert-danger fade" 
+							role="alert">
+								<h6 class="alert-heading">CPF incorreto</h6>
+								<small>(<?php echo $cpf;?>)</small>								
+								<div class="alert alert-body"><p>O CPF deve conter 11 números. Não informe caracteres especiais</p></div>
+							</div>
+							<div id ="alert_cpf_nao_cadastrado"class="alert alert-sm alert-success fade" 
+							role="alert">
+								<h6 class="alert-heading">CPF Não Cadastrado</h6>
+								<small>(<?php echo $cpf;?>)</small>								
+								<div class="alert alert-body"><p>O CPF informado não consta em nosso Banco de Dados. Você cadastrar os dados pessoais deste CPF</p></div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
---
+++ sistema/validador_cadastro_pf.php
@@ -0,0 +1,138 @@
+<?php
+require "../configuracoes.php";
+
+if(isset($_POST['validar_cpf']) && ($_POST['validar_cpf'] != "")){
+	$cpf=addslashes($_POST['validar_cpf']);
+	if(strlen ($cpf)==11){
+
+		$cpf=addslashes($_POST['validar_cpf']);
+		
+		$sql="SELECT * FROM pessoafisica WHERE cpf='$cpf'";
+
+		$sql=$pdo->query($sql);
+		if ($sql->rowCount() > 0){
+
+	        $dados_pf = $sql->fetch();
+			$nome_pf = $dados_pf['nome_pf'];
+			$email_pf = $dados_pf['email_pf'];
+			$cpf = $dados_pf['cpf'];
+			$rg_pf = $dados_pf['rg_pf'];
+			$telefone_pf = $dados_pf['telefone_pf'];
+			$endereco_rua_pf = $dados_pf['endereco_rua_pf'];
+			$endereco_numero_pf = $dados_pf['endereco_numero_pf'];
+			$endereco_complemento_pf = $dados_pf['endereco_complemento_pf'];
+			$endereco_bairro_pf = $dados_pf['endereco_bairro_pf'];
+			$endereco_cidade_pf = $dados_pf['endereco_cidade_pf'];
+			$endereco_estado_pf = $dados_pf['endereco_estado_pf'];
+			$endereco_cep_pf = $dados_pf['endereco_cep_pf'];
+			
+			$nasc_pf = date($dados_pf['nasc_pf']);
+			list($ano, $mes, $dia) = explode('-', $nasc_pf);
+			$nasc_pf2 = mktime(0,0,0, $mes, $dia, $ano);
+			$nasc_pf3 = date("d/m/Y", $nasc_pf2);
+		
+
+
+
+
+
+
+			?><script>$(document).ready(function(){
+			document.getElementById('alert_validacao').style.display = 'none';});</script>
+			<?php 	
+			?><script>$(document).ready(function(){
+			document.getElementById('alert_cpf_nao_cadastrado').style.display = 'none';});</script>
+			<?php 	
+			?><script>$(document).ready(function(){
+			document.getElementById('alert_cpf_incorreto').style.display = 'none';});</script>
+			<?php 	
+			?><script>$(document).ready(function(){
+			$('#alert_cpf_cadastrado').addClass('show');});</script>
+			<?php
+			?><script>$(document).ready(function(){
+			$('#botao_editar').addClass('btn-warning');
+			$('#botao_editar').removeAttr('disabled');});</script>
+			<?php 
+			?><script>$(document).ready(function(){
+			$('#botao_deletar').addClass('btn-danger');
+			$('#botao_deletar').removeAttr('disabled');});</script>
+			<?php 
+			?><script>$(document).ready(function(){
+			$('#botao_imprimir').addClass('btn-dark');
+			$('#botao_imprimir').removeAttr('disabled');});</script>
+			<?php 
+
+
+
+
+		
+			}else{
+				?><script>$(document).ready(function(){
+				document.getElementById('alert_validacao').style.display = 'none';});</script>
+				<?php 	
+				?><script>$(document).ready(function(){
+				document.getElementById('alert_cpf_cadastrado').style.display = 'none';});</script>
+				<?php 	
+				?><script>$(document).ready(function(){
+				document.getElementById('alert_cpf_incorreto').style.display = 'none';});</script>
+				<?php 	
+				?><script>$(document).ready(function(){
+				$('#alert_cpf_nao_cadastrado').addClass('show');});</script>
+				<?php
+				?><script>$(document).ready(function(){
+				$('#botao_cadastrar').addClass('btn-success');
+				$('#botao_cadastrar').removeAttr('disabled');});</script>
+				<?php 
+				}
+	}else{
+		?><script>$(document).ready(function(){
+		document.getElementById('alert_validacao').style.display = 'none';});</script>
+		<?php
+		?><script>$(document).ready(function(){
+		document.getElementById('alert_cpf_cadastrado').style.display = 'none';});</script>
+		<?php
+		?><script>$(document).ready(function(){
+		document.getElementById('alert_cpf_nao_cadastrado').style.display = 'none';});</script>
+		<?php 	
+		?><script>$(document).ready(function(){
+		$('#alert_cpf_incorreto').addClass('show');});</script>
+		<?php
+		?><script>$(document).ready(function(){
+		$('#validar_cpf').removeAttr('disabled');});</script>
+		<?php
+		?><script>$(document).ready(function(){
+		$('#botao_verificar').addClass('btn-primary');
+		$('#botao_verificar').removeAttr('disabled');});</script>
+		<?php 
+
+	}
+}else{
+	?><script>$(document).ready(function(){
+	$('#validar_cpf').removeAttr('disabled');});</script>
+	<?php 	
+	?><script>$(document).ready(function(){
+	document.getElementById('alert_cpf_incorreto').style.display = 'none';});</script>
+	<?php
+	?><script>$(document).ready(function(){
+	document.getElementById('alert_cpf_cadastrado').style.display = 'none';});</script>
+	<?php
+	?><script>$(document).ready(function(){
+	document.getElementById('alert_cpf_nao_cadastrado').style.display = 'none';});</script>
+	<?php 	
+	?><script>$(document).ready(function(){
+	$('#alert_validacao').addClass('show');});</script>
+	<?php 
+	?><script>$(document).ready(function(){
+	$('#botao_verificar').addClass('btn-primary');
+	$('#botao_verificar').removeAttr('disabled');});</script>
+	<?php 
+
+	}
+if(isset($_POST['deletar_cadastro']) && ($_POST['deletar_cadastro'] != "")){
+echo"oi";
+}else{
+
+}
+
+
+
